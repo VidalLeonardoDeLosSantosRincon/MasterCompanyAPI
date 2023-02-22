71616524dae062d194e0cf00ea12a55be1f7686e
VidalLeonardoDeLosSantosRincon <niggbibi@hotmail.com>
2023-02-22 19:04:46 -0400

    Adding method GetAll

--- DAOs/EmployeeDAO.cs
+++ DAOs/EmployeeDAO.cs
@@ -1,18 +1,38 @@
 ﻿using MasterCompanyAPI.Database;
 using MasterCompanyAPI.Models;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
-using System.Diagnostics;
 
 namespace MasterCompanyAPI.DAOs
 {
     public class EmployeeDAO
     {
-        private readonly Context db;
+        private readonly Context<Employee> db;
 
         public EmployeeDAO()
         {
             db = new("Employees", ".txt");
+        }
+
+        /// <summary>
+        ///     <c><see langword="async"/> method </c>
+        ///     <para>
+        ///         Returns a list of employees(All) 
+        ///     </para>
+        ///     <para>Uses:
+        ///         <code>- <see cref="Context{E}.JsonArray"/></code>
+        ///         <code>- <see cref="Context{E}.JsonToModelList"/></code>
+        ///     </para>
+        /// </summary>
+        /// <returns>
+        ///     <see cref="List{E}"/> of type <see cref="Employee"/>
+        /// </returns>
+        public async Task<List<Employee>> GetAll()
+        {
+            List<Employee> employees = new();
+
+            var results = db.JsonToModelList(await db.JsonArray());
+            if (results != null) employees = results;
+
+            return employees;
         }
     }
 }
