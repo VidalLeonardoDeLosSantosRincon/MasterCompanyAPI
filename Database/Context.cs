0aebdb6afa61b7f72ae95b4f787672845a8e4b67
VidalLeonardoDeLosSantosRincon <niggbibi@hotmail.com>
2023-02-22 00:33:25 -0400

    Adding method SetPath

--- Database/Context.cs
+++ Database/Context.cs
@@ -11,6 +11,31 @@
         public Context(string? filename, string? extension)
         {
             this.Results = new();
+            this.SetPath(filename, extension);
+        }
+
+        /// <summary>
+        /// Set the current file path.
+        /// </summary>
+        /// <param name="file_name">Specify the file name (without extension)</param>
+        /// <param name="file_extension">Specify the file extension
+        ///    (<see langword=".txt"/>, <see langword=".json"/>)
+        /// </param>
+        /// <returns>
+        /// <see langword="true"/> if everything is okey, otherwise
+        /// <see cref="ApplicationException"/> is something fail.
+        /// </returns>
+        public object SetPath(string? file_name, string? file_extension)
+        {
+
+            file_name = (file_name ?? "").Trim();
+            file_extension = (file_extension ?? "").Trim();
+
+            if (file_name == "") return new ApplicationException("Nombre de archivo no valido");
+            if (file_extension == "" || !Context.ValidExtension.Contains(file_extension)) return new ApplicationException("Extensión de archivo no valida");
+
+            Context.FilePath = string.Format(@"{0}\{1}{2}", Context.BasePath, file_name, file_extension);
+            return true;
         }
     }
 }
