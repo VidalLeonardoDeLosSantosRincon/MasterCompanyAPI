2aedeffb4189a88fbadb6e3dc724073bd1fa821b
VidalLeonardoDeLosSantosRincon <niggbibi@hotmail.com>
2023-02-22 15:07:06 -0400

    Configuring Employee DAO

---
+++ DAOs/EmployeeDAO.cs
@@ -0,0 +1,18 @@
+﻿using MasterCompanyAPI.Database;
+using MasterCompanyAPI.Models;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Diagnostics;
+
+namespace MasterCompanyAPI.DAOs
+{
+    public class EmployeeDAO
+    {
+        private readonly Context db;
+
+        public EmployeeDAO()
+        {
+            db = new("Employees", ".txt");
+        }
+    }
+}
