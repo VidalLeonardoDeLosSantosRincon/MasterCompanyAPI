7c4af353c5ec4ad916b754af88e1430d0ee95eaa
VidalLeonardoDeLosSantosRincon <niggbibi@hotmail.com>
2023-02-22 00:25:28 -0400

    Adding class Context

---
+++ Database/Context.cs
@@ -0,0 +1,16 @@
+﻿namespace MasterCompanyAPI.Database
+{
+    public class Context
+    {
+        private static readonly string BasePath = @"Database";
+        private static readonly List<string> ValidExtension = new() { ".txt", ".json" };
+        private static string? FilePath = null;
+
+        private List<object> Results { get; set; }
+
+        public Context(string? filename, string? extension)
+        {
+            this.Results = new();
+        }
+    }
+}
