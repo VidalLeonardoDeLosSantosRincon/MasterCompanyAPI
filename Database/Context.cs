9631999e11373fc0cb6ac7891b701f9e67ba1c3f
VidalLeonardoDeLosSantosRincon <niggbibi@hotmail.com>
2023-02-22 16:24:29 -0400

    Including DataFile

--- Database/Context.cs
+++ Database/Context.cs
@@ -1,15 +1,18 @@
 ﻿using MasterCompanyAPI.Models;
+using MasterCompanyAPI.Utils;
 
 namespace MasterCompanyAPI.Database
 {
     public class Context<E>
         where E : Employee
-    {  
+    {
+        private readonly DataFile dataFile;
         private List<E> Results { get; set; }
 
         public Context(string? filename, string? extension)
         {
             this.Results = new();
+            this.dataFile = new(filename, extension);
         }
 
     }
