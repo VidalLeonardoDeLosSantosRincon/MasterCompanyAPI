efab99abd23dd302f257b0981bfd3e2fe468790f
VidalLeonardoDeLosSantosRincon <niggbibi@hotmail.com>
2023-02-22 18:40:06 -0400

    Adding method GetContent

--- Database/Context.cs
+++ Database/Context.cs
@@ -6,7 +6,7 @@ namespace MasterCompanyAPI.Database
     public class Context<E>
         where E : Employee
     {
-        private readonly DataFile dataFile;
+        private readonly DataFile dataFile;   
         private List<E> Results { get; set; }
 
         public Context(string? filename, string? extension)
@@ -15,5 +15,19 @@ namespace MasterCompanyAPI.Database
             this.dataFile = new(filename, extension);
         }
 
+        /// <summary>
+        ///     <c><see langword="async"/> method </c>
+        ///    <para>Uses:
+        ///         <code>- <see cref="DataFile.ReadFile"/></code>
+        ///     </para>
+        /// </summary>
+        /// <returns>
+        ///     the text (<see langword="string"/>) read from the file 
+        ///     (<see langword="txt"/>, <see langword="json"/>)
+        /// </returns>
+        public async Task<string> GetContent()
+        {
+            return await dataFile.ReadFile();
+        }
     }
 }
