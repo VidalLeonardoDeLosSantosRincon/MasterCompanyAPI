689dd73d5f4c4a87b9d27d80b10a4b16e3a465e0
VidalLeonardoDeLosSantosRincon <niggbibi@hotmail.com>
2023-02-23 16:11:48 -0400

    Adding class EmployeeService

---
+++ Services/EmployeeService.cs
@@ -0,0 +1,13 @@
+﻿using MasterCompanyAPI.Repositories;
+
+namespace MasterCompanyAPI.Services
+{
+    public class EmployeeService
+    {
+        private readonly EmployeeRepository employeeRepo;
+      
+        public EmployeeService() {
+            employeeRepo = new EmployeeRepository();
+        }
+    }
+}
