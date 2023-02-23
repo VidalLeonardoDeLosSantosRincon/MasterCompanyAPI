39e26888e3a26675d1ea1f1d1242c9ef5fc345d1
VidalLeonardoDeLosSantosRincon <niggbibi@hotmail.com>
2023-02-22 23:19:54 -0400

    Removing fields Id and Status

--- Models/Employee.cs
+++ Models/Employee.cs
@@ -14,8 +14,7 @@ namespace MasterCompanyAPI.Models
     /// <see cref="IEmployee"/>
     /// </summary>
     public class Employee : IEmployee
-    {
-        public int Id { get; set; }
+    {  
         public string? Name { get; set; }
         public string? LastName { get; set; }
         public string? Document { get; set; }
@@ -23,6 +22,5 @@ namespace MasterCompanyAPI.Models
         public string? Gender { get; set; }
         public string? Position { get; set; }
         public string? StartDate { get; set; }
-        public int Status { get; set; }
     }
 }
