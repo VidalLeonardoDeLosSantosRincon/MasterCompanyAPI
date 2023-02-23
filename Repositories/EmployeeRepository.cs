e45f35d1cc385661e5b96cf869eb08b3eb6a485f
VidalLeonardoDeLosSantosRincon <niggbibi@hotmail.com>
2023-02-22 20:44:55 -0400

    Adding method GetEmployeeByDocument

--- Repositories/EmployeeRepository.cs
+++ Repositories/EmployeeRepository.cs
@@ -33,6 +33,30 @@ namespace MasterCompanyAPI.Repositories
         /// <summary>
         ///     <c><see langword="async"/> method </c>
         ///     <para>
+        ///         Returns a specific employee by his/her <see cref="Employee.Document"/>
+        ///     </para>
+        ///     <para>Uses:
+        ///         <code>- <see cref="EmployeeDAO.GetAll"/></code>
+        ///     </para>
+        /// </summary>
+        /// <returns>
+        ///     <see langword="object"/> of type <see cref="Employee"/>
+        /// </returns>
+        public async Task<Employee?> GetEmployeeByDocument(string? document)
+        {
+            Employee? employee = null;
+            document = (document ?? "").Trim();
+            if (document == "") return null;
+
+            List<Employee> employees = await employeeDao.GetAll();
+            employee = employees.Where(x => x.Document == document).FirstOrDefault();
+
+            return employee;
+        }
+
+        /// <summary>
+        ///     <c><see langword="async"/> method </c>
+        ///     <para>
         ///         Returns a list of employees(no duplicated) 
         ///     </para>
         ///     <para>Uses:
