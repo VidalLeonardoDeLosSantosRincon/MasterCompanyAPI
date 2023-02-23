60533a82cfeb0344877d804e4e0aa00d9614c3fd
VidalLeonardoDeLosSantosRincon <niggbibi@hotmail.com>
2023-02-23 16:55:18 -0400

    Adding method GetGenderPercentages

--- Services/EmployeeService.cs
+++ Services/EmployeeService.cs
@@ -1,4 +1,5 @@
-﻿using MasterCompanyAPI.Repositories;
+﻿using MasterCompanyAPI.Models;
+using MasterCompanyAPI.Repositories;
 
 namespace MasterCompanyAPI.Services
 {
@@ -33,6 +34,36 @@ namespace MasterCompanyAPI.Services
             double result = ((double)cuantity / (double)total) * 100;
             result = Math.Round(result, 2);
             return result;
+        }
+
+        /// <summary>
+        ///     Calculates the gender percentages of employees
+        ///     <para>Uses:
+        ///         <code>- <see cref="EmployeeRepository.GetEmployees()"/></code>
+        ///         <code>- <see cref="CalculatePercetange"/></code>
+        ///     </para>
+        /// </summary>
+        /// <returns>
+        ///     An <see langword="object"/> that contains the total gender percentages
+        /// </returns>
+        public async Task<Object> GetGenderPercentages()
+        {
+            List<Employee> employees = await employeeRepo.GetEmployees();
+            List<Employee> males = employees.Where(x => x.Gender != null && x.Gender.ToUpper().Equals("M")).ToList();
+            List<Employee> females = employees.Where(x => x.Gender != null && x.Gender.ToUpper().Equals("F")).ToList();
+
+            int total = employees.Count;
+            int total_males = males.Count;
+            int total_females = females.Count;
+            double males_percent = CalculatePercetange(total_males, total);
+            double females_percent = CalculatePercetange(total_females, total);
+
+            return new
+            {
+                all = new { total, percentage = 100 },
+                males = new { total = total_males, percentage = males_percent },
+                females = new { total = total_females, percentage = females_percent },
+            };
         }
 
     }
