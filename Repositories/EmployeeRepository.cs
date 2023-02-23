7d441de8b79e99ef9b25a4348ba60b4e5923c90f
VidalLeonardoDeLosSantosRincon <niggbibi@hotmail.com>
2023-02-22 21:22:19 -0400

    Adding method GetEmployeesBySalaryRange

--- Repositories/EmployeeRepository.cs
+++ Repositories/EmployeeRepository.cs
@@ -57,6 +57,25 @@ namespace MasterCompanyAPI.Repositories
         /// <summary>
         ///     <c><see langword="async"/> method </c>
         ///     <para>
+        ///         Returns a list of employees by a salary (<see cref="Employee.Salary"/>) range
+        ///     </para>
+        ///     <para>Uses:
+        ///         <code>- <see cref="EmployeeRepository.GetEmployees"/></code>
+        ///     </para>
+        /// </summary>
+        /// <returns>
+        ///     <see cref="List{E}"/> of type <see cref="Employee"/>
+        /// </returns>
+        public async Task<List<Employee>> GetEmployeesBySalaryRange(double? from_salary, double? to_salary)
+        {
+            List<Employee> employees = await this.GetEmployees();
+            employees = employees.Where(x => x.Salary >= from_salary && x.Salary <= to_salary).ToList();
+            return employees;
+        }
+
+        /// <summary>
+        ///     <c><see langword="async"/> method </c>
+        ///     <para>
         ///         Returns a list of employees(no duplicated) 
         ///     </para>
         ///     <para>Uses:
