1a5c9543bc284d0dfdaf13323252135411ae512a
VidalLeonardoDeLosSantosRincon <niggbibi@hotmail.com>
2023-02-23 21:09:43 -0400

    Adding method Delete

--- DAOs/EmployeeDAO.cs
+++ DAOs/EmployeeDAO.cs
@@ -74,5 +74,49 @@ namespace MasterCompanyAPI.DAOs
             return await db.AddContent(json);
 
         }
+
+        /// <summary>
+        ///     <c><see langword="async"/> method </c>,
+        ///     deletes an employee by his/her document
+        ///     <para>Uses:
+        ///         <code>- <see cref="Context{E}.AddContent"/></code>
+        ///         <code>- <see cref="Context{E}.JsonArray"/></code>
+        ///         <code>- <see cref="Context{E}.JsonToModelList"/></code>
+        ///         <code>- <see cref="JsonSerializer.Serialize"/></code>
+        ///     </para>
+        /// </summary>
+        /// <param name="document">
+        ///      Represents the <see langword="property"/> Document of the <see cref="Employee"/> that will be removed from the list 
+        ///      to update the file content.
+        /// </param>
+        /// <returns>
+        ///     <see langword="true"/> 
+        ///     if <see langword="param"/> <paramref name="document"/> is not <see langword="null"/>,
+        ///     
+        ///     and the content was removed from the file successfully,
+        ///     otherwise <see langword="false"/>.
+        /// </returns>
+        public async Task<bool> Delete(string? document)
+        {
+            if (document == null) return false;
+
+            List<Employee> employees = new();
+
+            var results = db.JsonToModelList(await db.JsonArray());
+            if (results == null) return false;
+
+            employees = results;
+            Employee? employee = employees.Where(x => x.Document != null && x.Document.Equals(document)).FirstOrDefault();
+
+            if (employee == null) return false;
+           
+            bool removed = employees.Remove(employee);
+            if (!removed) return false;
+
+            string json = JsonSerializer.Serialize(employees);
+            //Debug.WriteLine(json);
+
+            return await db.AddContent(json);
+        }
     }
 }
