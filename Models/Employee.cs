246a20e3f74f54503fd795e91101b01bc58126be
VidalLeonardoDeLosSantosRincon <niggbibi@hotmail.com>
2023-02-24 12:03:02 -0400

    Adding Data Annotations

--- Models/Employee.cs
+++ Models/Employee.cs
@@ -1,4 +1,6 @@
 ﻿using MasterCompanyAPI.Interfaces;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace MasterCompanyAPI.Models
 {
@@ -14,13 +16,40 @@ namespace MasterCompanyAPI.Models
     /// <see cref="IEmployee"/>
     /// </summary>
     public class Employee : IEmployee
-    {  
+    {
+        [Required]
+        [MinLength(1)]
+        [MaxLength(150)]
         public string? Name { get; set; }
+
+        [Required]
+        [MinLength(1)]
+        [MaxLength(200)]
         public string? LastName { get; set; }
+
+        [Required]
+        [MinLength(11)]
+        [MaxLength(11)]
         public string? Document { get; set; }
+
+        [Required]
+        [DefaultValue(0)]
         public double Salary { get; set; }
+
+        [Required]
+        [MinLength(1)]
+        [MaxLength(1)]
         public string? Gender { get; set; }
+
+        [Required]
+        [MinLength(1)]
         public string? Position { get; set; }
+
+        [Required]
+        [MinLength(10)]
+        [MaxLength(10)]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}")]
         public string? StartDate { get; set; }
     }
 }
